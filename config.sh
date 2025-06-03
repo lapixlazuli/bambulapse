89162eb5c7e97c3f02c9af1cff6e6e3dbdd5cdbf
thidiasr <thdramos@hotmail.com>
2025-06-03 19:55:50 -0300

    feat: Adicionando arquivo de configuração

---
+++ config.sh
@@ -0,0 +1,14 @@
+#webcam config
+VIDEODEVICE=0
+WIDTH=1920
+HEIGHT=1080
+BRIGHTNESS=40
+SATURATION=50
+PIXEL_FORMAT=8
+#shared folder directory
+DIR="/home/pi/TimelapseShare"
+#GPIO pins (using wiringpi numbering)
+GPIO_TRIG=2
+GPIO_ECHO=5
+#dependencie
+WIRINGPI_REPO="https://github.com/WiringPi/WiringPi.git"
